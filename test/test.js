3dd7c480aaf4d55d4cb4860261ab9839ad640080
Victor Felder <victor@draft.li>
2019-05-21 18:06:15 +0200

    adapt to node12

--- test/test.js
+++ test/test.js
@@ -1,187 +1,189 @@
 /* global describe it */
 
-var assert = require('assert')
-var express = require('express')
-var http = require('http')
-var patchHeaders = require('..')
-var request = require('supertest')
+const assert = require('assert')
+const express = require('express')
+const http = require('http')
+const patchHeaders = require('..')
+const request = require('supertest')
+
+const wrap = (obj) => Object.assign(Object.create(null), obj)
 
 describe('patch-headers', function () {
   describe('patch', function () {
-    it('should add empty headers object if it\'s null', function () {
-      var res = new http.ServerResponse({})
+    it("should add empty headers object if it's null", function () {
+      const res = new http.ServerResponse({})
 
       patchHeaders.patch({}, res)
 
-      assert.deepEqual(res._headers, {})
+      assert.deepStrictEqual(res.getHeaders(), wrap({}))
     })
 
-    it('should ignore allow if it\'s null', function () {
-      var res = new http.ServerResponse({})
+    it("should ignore allow if it's null", function () {
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({allow: null}, res)
+      patchHeaders.patch({ allow: null }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1',
         'b': '2'
-      })
+      }))
     })
 
     it('should remove headers not listed in allow', function () {
-      var res = new http.ServerResponse({})
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({allow: ['a']}, res)
+      patchHeaders.patch({ allow: ['a'] }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1'
-      })
+      }))
     })
 
     it('should ignore case of allow values', function () {
-      var res = new http.ServerResponse({})
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({allow: ['A']}, res)
+      patchHeaders.patch({ allow: ['A'] }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1'
-      })
+      }))
     })
 
-    it('should ignore remove if it\'s null', function () {
-      var res = new http.ServerResponse({})
+    it("should ignore remove if it's null", function () {
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({remove: null}, res)
+      patchHeaders.patch({ remove: null }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1',
         'b': '2'
-      })
+      }))
     })
 
     it('should remove headers listed in remove', function () {
-      var res = new http.ServerResponse({})
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({remove: ['a']}, res)
+      patchHeaders.patch({ remove: ['a'] }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'b': '2'
-      })
+      }))
     })
 
     it('should ignore case of remove values', function () {
-      var res = new http.ServerResponse({})
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({remove: ['A']}, res)
+      patchHeaders.patch({ remove: ['A'] }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'b': '2'
-      })
+      }))
     })
 
-    it('should ignore static if it\'s null', function () {
-      var res = new http.ServerResponse({})
+    it("should ignore static if it's null", function () {
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({static: null}, res)
+      patchHeaders.patch({ static: null }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1',
         'b': '2'
-      })
+      }))
     })
 
     it('should ignore static if the property value is null', function () {
-      var res = new http.ServerResponse({})
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({static: {a: null}}, res)
+      patchHeaders.patch({ static: { a: null } }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1',
         'b': '2'
-      })
+      }))
     })
 
     it('should add headers defined in static', function () {
-      var res = new http.ServerResponse({})
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({static: {'c': '3'}}, res)
+      patchHeaders.patch({ static: { 'c': '3' } }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1',
         'b': '2',
         'c': '3'
-      })
+      }))
     })
 
-    it('should ignore callback if it\'s null', function () {
-      var res = new http.ServerResponse({})
+    it("should ignore callback if it's null", function () {
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      patchHeaders.patch({callback: null}, res)
+      patchHeaders.patch({ callback: null }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '1',
         'b': '2'
-      })
+      }))
     })
 
     it('should use callback to change headers', function () {
-      var res = new http.ServerResponse({})
+      const res = new http.ServerResponse({})
 
       res.setHeader('a', '1')
       res.setHeader('b', '2')
 
-      var patch = function (headers) {
+      const patch = function (headers) {
         headers['a'] = (parseInt(headers['a']) + 2).toString()
 
         return headers
       }
 
-      patchHeaders.patch({callback: patch}, res)
+      patchHeaders.patch({ callback: patch }, res)
 
-      assert.deepEqual(res._headers, {
+      assert.deepStrictEqual(res.getHeaders(), wrap({
         'a': '3',
         'b': '2'
-      })
+      }))
     })
   })
 
   describe('middleware', function () {
-    it('should hijack the response and patch the headers', function (done) {
-      var app = express()
+    it("should hijack the response and patch the headers', function", (done) => {
+      const app = express()
 
       app.use(function (req, res, next) {
-        patchHeaders.middleware({static: {
+        patchHeaders.middleware({ static: {
           'a': '1'
-        }}, req, res, next)
+        } }, req, res, next)
       })
 
       app.use(function (req, res) {
@@ -196,10 +198,10 @@ describe('patch-headers', function () {
   })
 
   describe('factory', function () {
-    it('should build a middleware', function (done) {
-      var app = express()
+    it("should build a middleware', function", (done) => {
+      const app = express()
 
-      app.use(patchHeaders({static: {'a': '1'}}))
+      app.use(patchHeaders({ static: { 'a': '1' } }))
 
       app.use(function (req, res) {
         res.end('test')
